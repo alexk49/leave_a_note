89956d97612d230b6b89c475e635a4b238ccff46
alexk49 <alexcode9@mailbox.org>
2024-08-13 08:14:00 +0100

    updated to use urllib

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,18 +1,18 @@
 import unittest
-
-import requests
+from urllib import request
 
 from leave_a_note import app
 
 
-class TestApp(unittest.TestCase):
+class MyTestApp(unittest.TestCase):
     def setUp(self):
         # tests run on localhost server
         # which is set up from makefile
-        self.app_url = "http://localhost:8080/"
+        self.app_root = "http://localhost:8080/"
 
     def test_home_page_response(self):
         """Testing webpage response code"""
-        print(f"Testing page loads with correct status code")
-        response = requests.get(self.app_url)
-        self.assertEqual(response.status_code, 200)
+        print("Testing page loads with correct status code")
+
+        response = request.urlopen(self.app_root)
+        self.assertEqual(response.status, 200)
