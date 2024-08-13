244933c14785b68b17e4dac1052d5a892d5d4e12
alexk49 <alexcode9@mailbox.org>
2024-08-13 08:12:50 +0100

    updated app paths to use full links for static and views.
    This means the app still works when run from the root dir of the project, which
    allows tests to be run.

--- leave_a_note/app.py
+++ leave_a_note/app.py
@@ -1,6 +1,9 @@
 import os
 
-from bottle import route, run, template, static_file, TEMPLATE_PATH
+from bottle import route, run, template, static_file, TEMPLATE_PATH, SimpleTemplate, default_app
+
+
+abs_app_dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
 @route("/")
@@ -10,17 +13,18 @@ def index():
 
 @route("/static/<filename:path>")
 def server_static(filename):
-    return static_file(filename, root="static")
+    return static_file(filename, root=os.path.join(abs_app_dir_path, "static"))
 
 
-def run_local():
-    run(host="localhost", port=8080, debug=True, reloader=True)
+def main():
+    # ensure templates path is still findable when app is run from project root
+    abs_views_path = os.path.join(abs_app_dir_path, "views")
+    TEMPLATE_PATH.insert(0, abs_views_path)
+
+    app = default_app()
+    # SimpleTemplate.defaults["get_url"] = app.get_url
+    app.run(host="localhost", port=8080, debug=True, reloader=True)
 
 
 if __name__ == "__main__":
-
-    # ensure templates path is still findable when app is run from project root
-    abs_app_dir_path = os.path.dirname(os.path.realpath(__file__))
-    abs_views_path = os.path.join(abs_app_dir_path, "views")
-    TEMPLATE_PATH.insert(0, abs_views_path)
-    run_local()
+    main()
