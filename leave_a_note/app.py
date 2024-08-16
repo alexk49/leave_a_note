775e6a5af62d683151283bb02c738fb7f498240f
alexk49 <alexcode9@mailbox.org>
2024-08-16 09:02:23 +0100

    added functions to create new database if it doesn't exist, with tests to match creation and schema

--- leave_a_note/app.py
+++ leave_a_note/app.py
@@ -1,13 +1,19 @@
 import os
+import logging
+import sqlite3
 
 from bottle import route, run, template, static_file, TEMPLATE_PATH, default_app
 
+logging.basicConfig(level=logging.INFO, format=" %(asctime)s -  %(levelname)s -  %(message)s")
 
 abs_app_dir_path = os.path.dirname(os.path.realpath(__file__))
 abs_views_path = os.path.join(abs_app_dir_path, "views")
 TEMPLATE_PATH.insert(0, abs_views_path)
 
 app = default_app()
+
+DB_DIR_PATH = os.path.join(abs_app_dir_path, "data")
+DB_PATH = os.path.join(abs_app_dir_path, "data", "notes.db")
 
 
 @route("/")
@@ -16,7 +22,7 @@ def index():
 
 
 @route("/static/<filename:path>")
-def server_static(filename):
+def server_static(filename: str):
     """
     Required to load all static files
     Absolute dir path is used to handle app
@@ -25,9 +31,32 @@ def server_static(filename):
     return static_file(filename, root=os.path.join(abs_app_dir_path, "static"))
 
 
+def check_database_exists(db_dir_path: str, db_path: str):
+    """Check database and folder exists
+    and make folder and database if not"""
+    if os.path.exists(db_dir_path) is False:
+        logging.info("%s does not exist", db_path)
+        os.mkdir(db_dir_path)
+        create_new_database(db_path)
+
+
+def create_new_database(db_path: str):
+    """Create new database file with standard rows."""
+    logging.info("creating new database: %s", db_path)
+    connection = sqlite3.connect(db_path)
+    cursor = connection.cursor()
+    cursor.execute(
+        "CREATE TABLE notes(id INTEGER PRIMARY KEY AUTOINCREMENT, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, note TEXT NOT NULL)"
+    )
+    connection.commit()
+    connection.close()
+
+
 def main():
     # ensure templates path is still findable when app is run from project root
     app.run(host="localhost", port=8080, debug=True, reloader=True)
+
+    check_database_exists(DB_DIR_PATH, DB_PATH)
 
 
 if __name__ == "__main__":
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+import sqlite3
 import unittest
 
 from webtest import TestApp
@@ -6,10 +9,50 @@ from leave_a_note import app
 
 
 class MyTestApp(unittest.TestCase):
-    def setUp(self):
-        # tests run on localhost server
-        # which is set up from makefile
-        self.test_server = TestApp(app.app)
+    @classmethod
+    def setUpClass(cls):
+        """Run at start of all tests
+        tests run on localhost server
+        which is set up from makefile
+        """
+        cls.test_server = TestApp(app.app)
+        cls.test_db_dir_path = "test_data"
+        cls.test_db_name = "test_notes.db"
+        cls.test_db_path = os.path.join(cls.test_db_dir_path, cls.test_db_name)
+
+        cls.check_test_database_exists()
+
+    @classmethod
+    def tearDownClass(cls):
+        """Run at end of all tests"""
+        # delete test dir
+        shutil.rmtree(cls.test_db_dir_path)
+
+    @classmethod
+    def check_test_database_exists(cls):
+        """Used as utility method to create new test db
+        but also tests the app.check_database_exists function"""
+        assert not (os.path.exists(cls.test_db_path))
+
+        app.check_database_exists(cls.test_db_dir_path, cls.test_db_path)
+
+        assert os.path.exists(cls.test_db_path)
+
+    def test_database_schema(self):
+        """In order to execute commands you have to create a connection
+        and then a database cursor"""
+        connection = sqlite3.connect(self.test_db_path)
+        cursor = connection.cursor()
+        # query the schema for all tables
+        cursor.execute("SELECT sql FROM sqlite_master WHERE type='table';")
+        schemas = cursor.fetchall()
+
+        connection.close()
+
+        # this will include notes db schema
+        # and sqlite3_sequence table schema
+        # which is made when using autoincrement
+        self.assertEqual(len(schemas), 2)
 
     def test_home_page_response(self):
         """Testing webpage response code"""
